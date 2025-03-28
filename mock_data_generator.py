cf9398e9d7571a5df7c4658edf6dafe44253259f
TijsKok97 <kok@hotitem.nl>
2025-03-28 16:03:00 +0100

    Update mock_data_generator.py

--- mock_data_generator.py
+++ mock_data_generator.py
@@ -10,7 +10,7 @@ fake = Faker()
 st.title("📊 Let's mock it up!")
 
 # Welcome message
-st.markdown("### 📢 Welcome to the Advanced Mock Data Generator! 🎉\nCustomize your dataset with multiple sheets and flexible options.")
+st.markdown("### \nCustomize your dataset with multiple sheets and flexible options.")
 
 # User input: Number of sheets
 num_sheets = st.number_input("📑 Number of Sheets:", min_value=1, max_value=10, value=1)
