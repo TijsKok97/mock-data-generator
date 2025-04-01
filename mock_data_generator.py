c74685a98111df514096b24befd03c24369e9ebb
TijsKok97 <kok@hotitem.nl>
2025-04-01 16:59:51 +0200

    Update mock_data_generator.py

--- mock_data_generator.py
+++ mock_data_generator.py
@@ -7,7 +7,8 @@ import io
 
 # Set your OpenAI API key from Streamlit secrets
 openai.api_key = st.secrets["openai"]["api_key"]
-
+st.write(st.secrets)
+'''
 # Welcome message
 st.markdown("### 📊 Welcome to Dimensional Model Generator for Power BI 🚀")
 st.write("Define your dataset structure either manually or through AI-powered suggestions!")
